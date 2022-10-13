90585c5b587795c59e238b387fee3ace6de6dd5c
Влад Клюшкин <vladkliushkin09@gmail.com>
2022-10-14 01:50:10 +0300

    fixed prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -9,7 +9,6 @@ SECOND_NUMBER = 500
 def game_code():
     first_rand_number = randint(FIRST_NUMBER, SECOND_NUMBER)
     question = f'{first_rand_number}'
-    print(f'{first_rand_number}')
     for i in range(2, first_rand_number):
         if first_rand_number % i == 0:
             correct_answer = 'no'
