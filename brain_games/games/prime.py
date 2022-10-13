626208b5a613b36b583d899bbcdf96ace59b7e59
Влад Клюшкин <vladkliushkin09@gmail.com>
2022-10-14 02:44:27 +0300

    fixed prime game

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,11 +6,21 @@ FIRST_NUMBER = 1
 SECOND_NUMBER = 500
 
 
+from random import randint
+
+
+GAME_BEGINNING = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+FIRST_NUMBER = 1
+SECOND_NUMBER = 500
+
+
 def game_code():
     first_rand_number = randint(FIRST_NUMBER, SECOND_NUMBER)
     question = f'{first_rand_number}'
-    for i in range(2, first_rand_number):
+    correct_answer = ''
+    for i in range(2, int(first_rand_number ** 0.5) + 1):
         if first_rand_number % i == 0:
-            correct_answer = 'no'
-    correct_answer = 'yes'
+            correct_answer = "no"
+            break
+        correct_answer = 'yes'
     return question, correct_answer
